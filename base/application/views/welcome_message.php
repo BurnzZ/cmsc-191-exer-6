a97316654a5dcc1e1d662c3170f6cdefae1b21db
BurnzZ <kevinoxy@gmail.com>
2015-04-23 17:40:08 +0800

    added intial code from CI

---
+++ base/application/views/welcome_message.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Fruit -- BASE</title>
+
+	<link rel="stylesheet" type="text/css" href="./assets/styles/css/styles.css">
+
+	<script type="text/javascript" src="./assets/js/jquery-1.11.2.min.js"></script>
+	<script type="text/javascript" src="./assets/js/main.js"></script>
+</head>
+<body>
+
+<div id="container">
+	Welcome to fruit
+</div>
+
+</body>
+</html>
