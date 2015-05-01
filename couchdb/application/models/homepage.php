ddc7ad5840e19bdedfa86f8feb1bafbfb9b580fc
janetordillano <janet_ordillano@yahoo.com>
2015-05-01 20:33:19 +0800

    couchdb/application/libraries/couchReplicator.php

---
+++ couchdb/application/models/homepage.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Homepage extends CI_Model {
+
+    function __construct(){
+        parent::__construct();
+    }
+
+    function addFruit($input){
+    }
+}
